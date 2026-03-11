1ae0839e7374932352a01eb989c515d905e42516
Umang <umanggarg0210@gmail.com>
2026-03-11 23:38:29 +0530

    updated the code with minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+
 function App() {
   const [products, setProducts] = useState([]);
 
-  const fetchProducts = async () => {
-    const res = await axios.get("http://localhost:8080/api/products");
-    setProducts(res.data);
-  };
   useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get("http://localhost:8080/api/products");
+      setProducts(res.data);
+    };
+
     fetchProducts();
   }, []);
+
   return (
     <div>
-      {products.map((product) => (
-        <li>{product.name}</li>
-      ))}
+      <ul>
+        {products.map((product) => (
+          <li key={product._id}>{product.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
 export default App;
