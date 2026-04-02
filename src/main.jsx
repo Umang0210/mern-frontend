a80bf6b6f8050aefc2157ad2a5f9d6c488145e8a
Umang <umanggarg0210@gmail.com>
2026-04-02 14:08:47 +0530

    StrictMode and createRoot

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-// import App1 from './components/App1.jsx'
+import App from './App'
 import "./index.css"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
